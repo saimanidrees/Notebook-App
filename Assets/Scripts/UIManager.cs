fcb11cb8c71828ce181c86f993bb3f1bfbad3b41
Saiman Idrees <amirsaiman775@gmail.com>
2026-03-08 00:33:33 -0800

    Starting Basic Architecture

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [Header("Screens")]
+    [SerializeField] private MainScreen mainScreen;
+    [SerializeField] private CreationScreen creationScreen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
